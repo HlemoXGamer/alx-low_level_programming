8721d8f250ea0ae0c71bae2d2ad6ba071ee67dd3
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-08 03:02:33 -0700

    Done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase then uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = getchar();
+
+	for (c = 'a'; z <= 'z'; c++)
+		putchar(c);
+	
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	purchar('\n');
+
+	return (0);
+}
