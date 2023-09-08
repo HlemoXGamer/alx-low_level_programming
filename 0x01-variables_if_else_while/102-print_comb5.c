600742250520188131c02e5d1e652667b84145be
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-08 03:48:33 -0700

    Done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible 2 digits numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 0;
+
+	for (n1 = 0; n1 < 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 99; n2++)
+		{
+			putchar(n1 + '0');
+			putchar(n1 + '0');
+			putchar(' ');
+			putchar(n2 + '0');
+			putchar(n2 + '0');
+
+			if (n1 == 98 && n2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
