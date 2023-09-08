31aa2a7a27d485be959a6a6122b40dd850218369
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-08 03:39:15 -0700

    Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 	int n1 = 0;
 	int n2 = 0;
 
-	for (n1 = 0; n1 < 10; n1++)
+	for (n1 = 0; n1 < 9; n1++)
 	{
 		for (n2 = 0; n2 < 10; n2++)
 		{
 			putchar(n1 + '0');
 			putchar(n2 + '0');
 
-			if (n1 == 9 && n2 == 9)
+			if (n1 == 8 && n2 == 9)
 				continue;
 
 			putchar(',');
