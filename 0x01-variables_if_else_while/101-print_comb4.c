f3310aaf4647ad6b548489414aa66ace9c99f3e6
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-08 03:44:04 -0700

    Done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible 3 digits numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n1 = 0;
+	int n2 = 0;
+	int n3 = 0;
+
+	for (n1 = 0; n1 < 8; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 9; n2++)
+		{
+			for (n3 = n2 + 1; n3 < 10; n3++)
+			{	
+				putchar(n1 + '0');
+				putchar(n2 + '0');
+				putchar(n3 + '0');
+
+				if (n1 == 7 && n2 == 2 && n3 == 3)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
