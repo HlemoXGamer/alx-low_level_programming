0e5ae5bdd14216fd52d9cf45360a5de18d71f0e3
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-08 02:54:08 -0700

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print a char followed by break line
+ * Return: 0 if main ran successfully
+ */
+int main(void)
+{
+	char c = getchar();
+
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
