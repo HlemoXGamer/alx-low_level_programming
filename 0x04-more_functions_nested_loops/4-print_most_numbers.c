cdd8f1f7b6f2edfff8c9a2482a9c5cc5c4227f37
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-14 18:53:16 -0700

    Done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print all decimal digits expect 2 and 4
+ * print_most_ibers - print all decimal digits expect 2 and 4
  * _putchar - print a char
  * Return: Always 0
  */
 
-void print_most_numbers(void)
+void print_most_ibers(void)
 {
 	int i = 0;
 
-	do {
-		if ((i + 48) == (2) || (i + 48) == (4))
+	for (i = 0; i <= 9; ++i)
+	{
+		if (i == 2 || i == 4)
 			continue;
-
 		_putchar(i + 48);
-		i++;
-	} while (i >= 0 && i <= 9);
+	}
 
 	_putchar('\n');
 }
