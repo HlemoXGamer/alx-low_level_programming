3e7b4a44d0c791eee5d15adce0acf6d4db1bcc09
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-14 18:41:03 -0700

    Done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,12 @@
 /**
  * print_number - print all decimal digits
  * _putchar - print a char
+ * Return: Always 0
  */
 
 void print_numbers(void)
 {
-	int i;
+	int i = 0;
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i);
