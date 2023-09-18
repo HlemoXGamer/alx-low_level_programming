1b7ed4f0847415b5b00941656e4e170225fd8e62
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-18 09:21:16 -0700

    Done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,8 @@ void print_rev(char *s)
 {
 	int strLength;
 
-	for (strLength = 0; s[strLength] != '\0'; strLength++);
+	for (strLength = 0; s[strLength] != '\0'; strLength++)
+		;
 
 	for (--strLength; strLength >= 0; --strLength)
 		_putchar(s[strLength]);
